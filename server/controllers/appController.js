c0952d6262d9d91d977620750edca8ad504fa047
Ed soto <edsghot@gmail.com>
2023-04-07 20:02:53 -0500

    feat: agregando para generar OTP y verificar

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -2,6 +2,8 @@ import UserModel from '../model/User.model.js'
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 import ENV from '../config.js'
+import otpGenerator from 'otp-generator'
+
 
 /**midleware for verify user */
 
@@ -18,7 +20,6 @@ export async function verifyUser(req, res, next) {
         return res.status(404).send({ error: "Authentication Error" })
     }
 }
-
 
 export async function register(req, res) {
     try {
@@ -135,15 +136,24 @@ export async function updateUser(req, res) {
 }
 
 export async function generateOTP(req, res) {
-    res.json('generateOTP route')
+    req.app.locals.OTP = await otpGenerator.generate(6,{lowerCaseAlphabets: false,upperCaseAlphabets: false,specialChars: false})
+    res.status(201).send({code: req.app.locals.OTP})
 }
 
 export async function verifyOTP(req, res) {
-    res.json('verifyOTP route')
+    const {code} = req.query;
+    if(parseInt(req.app.locals.OTP) === parseInt(code)){
+        req.app.locals.OTP = null; // reseteamos el valor del OTP
+        req.app.locals.resetSession = true;
+        return res.status(201).send({msg: 'Verify Successfully!'})
+    }
+    return res.status(400).send({error: "Invalid OTP"})
 }
 
 export async function createResetSession(req, res) {
-    res.json('createResetSession route')
+    if(req.app.locals.resetSession) {
+        
+    }
 }
 
 export async function resetPassword(req, res) {
--- server/router/route.js
+++ server/router/route.js
@@ -3,7 +3,7 @@ const router = Router();
 
 /**importamos los controladores */
 import * as controller from "../controllers/appController.js"
-import Auth from "../middleware/auth.js";
+import Auth,{localVariables} from "../middleware/auth.js";
 
 /*POST Methods*/
 router.route('/register').post(controller.register)
@@ -13,12 +13,12 @@ router.route('/login').post(controller.verifyUser,controller.login);
 
 /**GET  Methods*/
 router.route('/user/:username').get(controller.getUser)
-router.route('/generateOTP').get(controller.generateOTP) //para generar de manera random el OTP
+router.route('/generateOTP').get(controller.verifyUser,localVariables,controller.generateOTP) //para generar de manera random el OTP
 router.route('/verifyOTP').get(controller.verifyOTP) //verificar el OTP
 router.route('/createResetSession').get(controller.createResetSession) //RESetear las variables
 
 /**PUT Methods s*/
-router.route('/updateuser').put(controller.updateUser)
+router.route('/updateuser').put(Auth,controller.updateUser)
 router.route('/resetPassword').put(controller.resetPassword)
 
 
